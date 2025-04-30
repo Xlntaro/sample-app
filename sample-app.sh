ef85da21c5cad2659422885ea823cae7864da745
Sample User <sample@example.com>
2025-04-30 10:42:04 +0000

    lll

--- sample-app.sh
+++ sample-app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+rm -rf tempdir
+mkdir -p tempdir/templates tempdir/static
+
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
